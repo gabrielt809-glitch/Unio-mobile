bd244ee828dd6455ae5a72931442ac4eec24137c
gabrielt809-glitch <gabrielt809@gmail.com>
2026-05-05 15:53:08 -0300

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-/* Unio Base Organizada v8.4 */
-/* Bibliotecas internas removidas.
-   Nutrição e Saúde agora funcionam como diários livres, sem listas fixas de alimentos/atividades. */
-const FDB=[];
-const ACTL=[];
